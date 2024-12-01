13a02d6489f03912986ee4fd08c711f17c4a9e7b
mucycle-kelompok4 <wadawwadaw204@gmail.com>
2024-12-01 09:30:31 +0700

    update readme

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './components/quotes.scss'
+import './components/quotes.scss';
 import Quotes from './components/quotes';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,8 +11,4 @@ root.render(
     <Quotes/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
